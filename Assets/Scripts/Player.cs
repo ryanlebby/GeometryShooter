110a3b9ba521e6d7edc3d647d426266086989b9b
ryanlebby <ryanlebby@gmail.com>
2018-05-06 21:03:22 -0300

    Added Roads

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float Health;
-    public float ProjectileSpeed;
+    
     public float HitBoxRadius;
     public LayerMask mask = 1 << 8;
-    public GameObject Projectile;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+    
 	
 	// Update is called once per frame
 	void Update () {
@@ -28,16 +23,6 @@ public class Player : MonoBehaviour {
                 Destroy(hit.transform.gameObject);
             }
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject go = Instantiate(
-                Projectile, 
-                transform.position + transform.forward * 2f, 
-                transform.rotation
-            );
-
-            go.GetComponent<Rigidbody>().velocity = go.transform.forward * ProjectileSpeed;
-        }
+        
 	}
 }
---
+++ Assets/Scripts/Player_Gun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Gun : MonoBehaviour {
+
+    public GameObject Projectile;
+    public float ProjectileSpeed;
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(
+                Projectile,
+                transform.position + transform.forward * 2f,
+                transform.rotation
+            );
+
+            go.GetComponent<Rigidbody>().velocity = transform.forward * ProjectileSpeed;
+        }
+
+    }
+}
